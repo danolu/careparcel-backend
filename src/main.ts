9c6a65883e6799d42e63dc93c5917310bd43b0e8
Daniel Oluborode <danoluborode@gmail.com>
2025-06-26 14:32:12 +0100

    edit dockerFile

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'https://careparcel.vercel.app',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
