17ab77c2771f41b0d876e32fa8431427d3d7e543
Daniel Oluborode <danoluborode@gmail.com>
2025-06-26 14:42:35 +0100

    add GCP actual port to Dockerfile

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
     origin: 'https://careparcel.vercel.app',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
